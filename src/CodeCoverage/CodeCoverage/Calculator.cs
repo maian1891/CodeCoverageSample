0cc10fa40422f7282338766c4748c17700071586
anmt <an.mai@infodation.vn>
2021-10-04 15:31:13 +0700

    Update Add feature

--- src/CodeCoverage/CodeCoverage/Calculator.cs
+++ src/CodeCoverage/CodeCoverage/Calculator.cs
@@ -4,6 +4,11 @@
     {
         public int Add(int one, int two)
         {
+            if (one == 0)
+            {
+                return 0 + two;
+            }
+
             return one + two;
         }
 
